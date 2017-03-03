ff21f656dcd990d8f9ed03dd6171346b667a42d4
Praveen Deshmukh <pdeshmukh@grepruby.com>
2017-03-04 01:53:27 +0530

    Implement Functionality of detail view

--- iOSTestTask/iOSTestTask/DetailViewController.swift
+++ iOSTestTask/iOSTestTask/DetailViewController.swift
@@ -2,20 +2,41 @@
 //  DetailViewController.swift
 //  iOSTestTask
 //
-//  Created by Pushpendra Mishra on 04/03/17.
-//  Copyright © 2017 Pushpendra Mishra. All rights reserved.
+//  Created by Praveen on 04/03/17.
+//  Copyright © 2017 Praveen. All rights reserved.
 //
 
 import UIKit
 
 class DetailViewController: UIViewController {
 
+    var userDetails = [String: Any]()
+    
+    @IBOutlet weak var imgUser: UIImageView!
+    @IBOutlet weak var lblGender: UILabel!
+    @IBOutlet weak var lblDOB: UILabel!
+    @IBOutlet weak var lblPhoneNo: UILabel!
+    @IBOutlet weak var lblEmail: UILabel!
+    @IBOutlet weak var lblName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupLoginUserDetails ()
         // Do any additional setup after loading the view.
     }
 
+    func setupLoginUserDetails () {
+        imgUser.layer.cornerRadius = imgUser.frame.size.width/2
+        imgUser.clipsToBounds = true
+        
+         self.imgUser.image = self.userDetails["profilepic"] as! UIImage?
+        self.lblName.text = String(format:"Name: %@", (self.userDetails["username"] as! String?)!)
+        self.lblEmail.text =  String(format:"Email: %@",(self.userDetails["email"] as! String?)!)
+        self.lblPhoneNo.text = String(format:"PhoneNumber: %@",(self.userDetails["phonenumber"] as! String?)!)
+        self.lblDOB.text = String(format:"DateOfBirth: %@",(self.userDetails["dateofbirth"] as! String?)!)
+        self.lblGender.text = String(format:"Gender: %@",(self.userDetails["gender"] as! String?)!)
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
